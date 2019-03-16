4fef06821a1de53db2c55a03567829b82d94488a
e1se <elsewhere997@163.com>
2019-03-16 11:54:01 +0800

    测试git封装

--- hexopush.bat
+++ hexopush.bat
@@ -1,8 +1,14 @@
+:input
+set /p des=输入提交描述ENTER结束:
+
 echo "begin it ..."
 E:
 cd E:\AElseworld\Code\Github\myfirst_hexo
 git add .
-git commit -m "$*"
-echo $*
+git commit -m "%des%"
+echo 正在提交:%des%
 git push origin master
 echo "finshished!"
+
+pause>nul
+cls & goto input
